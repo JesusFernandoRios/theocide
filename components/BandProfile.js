9508ba5f40e6eea0a217b0cf7c2dab12dc411828
jesus <Jesusfernandorios93@gmail.com>
2021-05-07 16:42:23 -0700

    created separate component for artist img and name. added bio provided by client anneed to add proper styling

---
+++ components/BandProfile.js
@@ -0,0 +1,12 @@
+
+import styles from '../styles/BandProfiles.module.css'
+function BandProfile({image, name}) {
+    return (
+        <div className={styles.container}>
+            <img src={image} alt={name}/>
+            <h2>{name}</h2>
+        </div>
+    )
+}
+
+export default BandProfile;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
 import Head from 'next/head'
+import BandProfile from '../components/BandProfile'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+
+  const bandMembers =[
+    { image:'/theo1.jpg',
+      name: 'bandmemberone'
+    },
+    {
+      image:'/theo2.jpg',
+      name: 'bandmemberone'
+    },
+    {
+      image:'/theo3.jpg',
+      name: 'Alex Angulo',
+      instrument:'Drums/ Vocals'
+    },
+    {
+      image:'/theo4.jpg',
+      name: 'bandmemberone'
+    }
+  ]  
+
   return (
 
     <div className={styles.container}>
@@ -14,9 +36,19 @@ export default function Home() {
 
       <main className={styles.main}>
         <article className={styles.mainArticle}>
-          <h1 className={styles.bandMembers}>Welcome To Theocide</h1>
+          <h1 className={styles.bandMembers}>The•ism</h1>
 
-          <p className={styles.bandNames}> 
+          <p> 
+              Belief in the existence of a god or gods, especially belief in a personal God as creator and ruler of the world
+              –cide ending originates from the Latin word caedere meaning to kill. 
+              We want the world to know that we are comming for there gods and religion. We are comming for there ignorance they hold on so dearly to. It shall be eliminated. 
+              Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
+              This is why we must kill god and religion for the cancer that it is.
+
+              Your Ignorance keeps you blind bring upon Theocide.
+            </p>
+
+          {/* <p className={styles.bandNames}> 
               Steven Ford - Rhythm Guitar |
               
               Tim Janes - Lead Guitar |
@@ -24,12 +56,16 @@ export default function Home() {
               Alex Angulo - Drums/ Vocals |
               
               Mario Ramos - Bass Guitar
-          </p>
+          </p> */}
         </article>
-        <figure className={styles.mainFigure}>
-          <img className={styles.bandImage} src="/bandPicture.jpg" alt="Band Picture" />
-        </figure>
       </main>
+
+      <section className={styles.profileSection}>
+        {bandMembers.map(members => (
+          <BandProfile image={members.image} name={members.name}/>
+        ))}
+
+      </section>
 
       <footer className={styles.footer}>
         <img src="/bandLogo.png" alt="Theocide Logo" className={styles.logo} />

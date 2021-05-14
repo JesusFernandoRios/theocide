78c2c4596d546f251eaa8ec16ac203a2b942ebad
jesus <Jesusfernandorios93@gmail.com>
2021-05-14 16:15:53 -0700

    changed groupPicture size and theocideContent height

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,8 @@ export default function Home() {
             Belief in the existence of a god or gods, especially belief in a personal God as creator and ruler of the world
             –cide ending originates from the Latin word caedere meaning to kill. 
             We want the world to know that we are comming for there gods and religion. We are comming for there ignorance they hold on so dearly to. It shall be eliminated. 
-            Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
+            Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. 
+            Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
             This is why we must kill god and religion for the cancer that it is.
             <br/>
             Your Ignorance keeps you blind bring upon Theocide.

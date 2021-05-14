4fc96f3d96f48fd7d491a6e7dfe351e3591a2833
jesus <Jesusfernandorios93@gmail.com>
2021-05-14 15:43:20 -0700

    working on group bio and motto

--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,12 @@ export default function Home() {
         <link rel="icon" href="/bandLogo.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>;
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Metal+Mania&display=swap" rel="stylesheet"></link>
       </Head>
 
       <section className={styles.main}>
 
-        <h1 className={styles.bandMotto}>Explore Theocide</h1>
+        <h1 className={styles.bandMotto}>Upcoming Events</h1>
+        <h3>Coming Soon...</h3>
 
         <hr className={styles.lineBreak}/>
         
@@ -67,6 +66,28 @@ export default function Home() {
             <InstagramIcon style={{ fontSize: 50, color:'white' }} />
           </a>
       </section>
+      
+      <article className={styles.bandBio}>
+
+        <img  className={styles.groupPicture} src='/groupPicture1.jpg' alt='Group Picture' />
+        
+        <div className={styles.theocideContent}>
+          <h2>the•ism</h2>
+
+          <p>
+            Belief in the existence of a god or gods, especially belief in a personal God as creator and ruler of the world
+            –cide ending originates from the Latin word caedere meaning to kill. 
+            We want the world to know that we are comming for there gods and religion. We are comming for there ignorance they hold on so dearly to. It shall be eliminated. 
+            Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
+            This is why we must kill god and religion for the cancer that it is.
+            <br/>
+            Your Ignorance keeps you blind bring upon Theocide.
+
+          </p>
+
+        </div>
+        
+      </article>
 
       <section className={styles.profileSection}>
         {bandMembers.map((members, index) => (

63ba392b32f278a80970c7285ab4095ce8b1a5cb
jesus <Jesusfernandorios93@gmail.com>
2021-06-04 14:11:07 -0700

    working on band bio page.

--- pages/Bio.js
+++ pages/Bio.js
@@ -1,22 +1,33 @@
+import NavBar from '../components/NavBar'
+
 import styles from '../styles/BioPage.module.css'
 
 function Bio() {
     return (
         <section className={styles.container}>
 
+            <NavBar/>
+
             <h1>The•ism</h1>
 
-            <p>
-            Belief in the existence of a god or gods, especially belief in a personal God as creator and ruler of the world
-            –cide ending originates from the Latin word caedere meaning to kill. 
-            We want the world to know that we are comming for there gods and religion. We are comming for there ignorance they hold on so dearly to. It shall be eliminated. 
-            Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. 
-            Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
-            This is why we must kill god and religion for the cancer that it is.
-            <br/>
-            Your Ignorance keeps you blind bring upon Theocide.
+            <div className={styles.content}>
 
-          </p>
+                <img src="/groupPicture1.jpg" alt="Group Picture"/>
+
+                <p>
+                    Belief in the existence of a god or gods, especially belief in a personal God as creator and ruler of the world
+                    –cide ending originates from the Latin word caedere meaning to kill. 
+                    We want the world to know that we are comming for there gods and religion. We are comming for there ignorance they hold on so dearly to. It shall be eliminated. 
+                    Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. 
+                    Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
+                    This is why we must kill god and religion for the cancer that it is.
+                    <br/>
+                    Your Ignorance keeps you blind. Bring upon Theocide.
+
+                </p>
+
+            </div>
+
         </section>
     )
 }

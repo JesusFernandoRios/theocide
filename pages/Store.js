eded1ea36f7ae2a92eb3900d9cbd8bb32d7b41af
jesus <Jesusfernandorios93@gmail.com>
2021-06-04 14:18:36 -0700

    updated store. coming soon notification

--- pages/Store.js
+++ pages/Store.js
@@ -1,9 +1,15 @@
-import React from 'react'
+
+import NavBar from '../components/NavBar'
+import styles from '../styles/StorePage.module.css'
 
 function Store() {
     return (
-        <div>
+        <div className={styles.container}>
+
+            <NavBar/>
+
             <h1>Store</h1>
+            <p>Coming Soon...</p>
         </div>
     )
 }

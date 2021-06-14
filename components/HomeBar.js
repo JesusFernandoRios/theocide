9a5a7e9d404ea9331548b85e77c2d03b871569a6
jesus <Jesusfernandorios93@gmail.com>
2021-06-13 21:16:06 -0700

    creating hamburger navbar for mobile version

--- components/HomeBar.js
+++ components/HomeBar.js
@@ -4,6 +4,11 @@ import styles from '../styles/HomeNav.module.css'
 function HomeBar() {
     return (
         <nav className={styles.container}>
+            <div className={styles.hamburger}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
             <ul className={styles.navRoutes}>
                 <li><Link href="/Events">Events</Link></li>
                 <li><Link href="/Band">Band</Link></li>
--- components/NavBar.js
+++ components/NavBar.js
@@ -4,15 +4,15 @@ import styles from '../styles/NavBar.module.css'
 function NavBar() {
     return (
         <nav className={styles.container}>
-            <ul className={styles.navLinks}> 
+            <ul className={styles.navLinks}>
+                <li>
+                    <Link href="/"><img src="/bandLogo.png" alt="band logo"/></Link>
+                </li>
                 <li>
                     <Link href="/Store"><a>lStore</a></Link>
                 </li>
                 <li>
                     <Link href="/Events"><a>lEvents</a></Link>
-                </li>
-                <li>
-                    <Link href="/"><img src="/bandLogo.png" alt="band logo"/></Link>
                 </li>
                 <li>
                     <Link href="/Band"><a>lBand</a></Link>

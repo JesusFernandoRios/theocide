38907ae622ce52eed03a77e4ab3ea7a9ddbce2d7
jesus <Jesusfernandorios93@gmail.com>
2021-06-16 07:57:58 -0700

    updating band profiles page to fit mobile devices and added logo image for background

--- pages/Band.js
+++ pages/Band.js
@@ -1,17 +1,18 @@
 
-import NavBar from '../components/NavBar'
+import NavBar from '../components/HomeBar'
 import BandProfile from "../components/BandProfile"
 import styles from '../styles/BandProfiles.module.css'
+import Footer from '../components/Footer'
 
 const bandMembers =[
-    { image:'/theo1.jpg',
-      name: 'bandmemberone',
+    { image:'/theo4.jpg',
+      name: 'Mario Ramos',
       role: 'Guitar'
     },
     {
       image:'/theo2.jpg',
-      name: 'bandmemberone',
-      role: 'Guitar'
+      name: 'Steven Ford',
+      role: 'Guitar/ Vocals'
     },
     {
       image:'/theo3.jpg',
@@ -19,26 +20,31 @@ const bandMembers =[
       role:'Drums/ Vocals'
     },
     {
-      image:'/theo4.jpg',
-      name: 'Mario Ramos',
+      image:'/theo5.jpg',
+      name: 'Santo Murillo',
       role: 'Bass Guitar'
     }
 ]  
 
 function Band() {
     return (
-      <section id="Members" className={styles.mainContainer}>
-        <NavBar/>
+      <>
+        <section id="Members" className={styles.mainContainer}>
+          <NavBar/>
 
-        <div className={styles.imageContainer}>
-          {bandMembers.map((members, index) => (
-            <BandProfile key={index} image={members.image} name={members.name} role={members.role}/>
-          ))}
-        </div>
+          <div className={styles.logoContainer}>
+            <img className={styles.colorLogo} src='/colorLogo/yellow.png' alt='Logo'/>
+          </div>
 
+          <div className={styles.imageContainer}>
+            {bandMembers.map((members, index) => (
+              <BandProfile key={index} image={members.image} name={members.name} role={members.role}/>
+            ))}
+          </div>
 
-      </section>
-      
+        </section>
+        <Footer/>
+      </>
     )
 }
 
--- pages/Bio.js
+++ pages/Bio.js
@@ -1,42 +1,47 @@
-import NavBar from '../components/NavBar'
-
+import NavBar from '../components/HomeBar'
+import Footer from '../components/Footer'
 import styles from '../styles/BioPage.module.css'
 
 function Bio() {
     return (
-        <section className={styles.container}>
+        <>
+            <section className={styles.container}>
 
-            <NavBar/>
+                <NavBar/>
+                <div className={styles.logoContainer}>
+                        <img className={styles.colorLogo} src='/colorLogo/red.png' alt='Logo'/>
+                </div>
 
-            <h1>Theocide</h1>
+                <div className={styles.content}>
 
-            <div className={styles.content}>
+                    <img src="/groupPicture1.jpg" alt="Group Picture"/>
+                    
+                    <div>
+                        <h2>Theocide</h2>
+                        <p>
+                        To murder the Gods.
 
-                <img src="/groupPicture1.jpg" alt="Group Picture"/>
+                        Theocide is the systematic eradication of 
+                        a certain religion. 
+                        We want the Christian theist world to know that we are coming for their God and religion. We are coming for their ignorance they hold on so dearly to. 
+                        It shall be eliminated. Religion and the belief in God and or Gods leads to a weakened critical thinking or allows one to accept things the way they are. 
+                        When ultimately one will learn one must think for oneself! Conspiracy, end of time predictions have allowed the churches to rule through fear, politics and 
+                        crusades for far too long. Religion breeds racism, sexism, bigotry and hatred.
+                        During The Conquistadors' Crusades through the Americas, they committed theocide to well established civilizations such as the Aztecs and Native Americans and 
+                        murdered their gods. This is theocide for the worse due to a Christian theism. That is why our music's message has a strong anti-Christian/anti- organized religion 
+                        stance as Christian theology has repeatedly bred contempt and ignorance and has allowed cultures to wipe out other civilizations' cultures without any regard. 
+                        This is why we must kill the idea of singular "God" and religion for the cancer that it is.
+                        <br/>
+                        Your ignorance keeps you blind. Bring upon Theocide.
 
-                <p>
+                    </p>
+                    </div>
 
-                    To murder the Gods.
+                </div>
 
-                    Theocide is the systematic eradication of 
-                    a certain religion. 
-                    We want the Christian theist world to know that we are coming for their God and religion. We are coming for their ignorance they hold on so dearly to. 
-                    It shall be eliminated. Religion and the belief in God and or Gods leads to a weakened critical thinking or allows one to accept things the way they are. 
-                    When ultimately one will learn one must think for oneself! Conspiracy, end of time predictions have allowed the churches to rule through fear, politics and 
-                    crusades for far too long. Religion breeds racism, sexism, bigotry and hatred.
-                    During The Conquistadors' Crusades through the Americas, they committed theocide to well established civilizations such as the Aztecs and Native Americans and 
-                    murdered their gods. This is theocide for the worse due to a Christian theism. That is why our music's message has a strong anti-Christian/anti- organized religion 
-                    stance as Christian theology has repeatedly bred contempt and ignorance and has allowed cultures to wipe out other civilizations' cultures without any regard. 
-                    This is why we must kill the idea of singular "God" and religion for the cancer that it is.
-                    <br/>
-                    Your ignorance keeps you blind. Bring upon Theocide.
-
-
-                </p>
-
-            </div>
-
-        </section>
+            </section>
+            <Footer/>
+        </>
     )
 }
 

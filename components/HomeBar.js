7a7a2dd7d0980afeae3d523430f16a8e44f0e738
jesus <Jesusfernandorios93@gmail.com>
2021-06-15 21:53:06 -0700

    updated navbar added store link

--- components/HomeBar.js
+++ components/HomeBar.js
@@ -19,10 +19,11 @@ function HomeBar() {
                 <div className={styles.line}></div>
             </div>
             <ul className={isActive ? styles.open : styles.navRoutes}>
+                <li><Link href="/">Home</Link></li>
                 <li><Link href="/Events">Events</Link></li>
                 <li><Link href="/Band">Band</Link></li>
                 <li><Link href="/Bio">Bio</Link></li>
-                <li><Link href="/Store">Store</Link></li>
+                <li><a href="https://www.facebook.com/theocide/shop?rid=294842914636307&rt=6&referral_code=page_shop_card" target="_blank">Store</a></li>
             </ul>
         </nav>
     )
--- pages/Bio.js
+++ pages/Bio.js
@@ -8,21 +8,29 @@ function Bio() {
 
             <NavBar/>
 
-            <h1>The•ism</h1>
+            <h1>Theocide</h1>
 
             <div className={styles.content}>
 
                 <img src="/groupPicture1.jpg" alt="Group Picture"/>
 
                 <p>
-                    Belief in the existence of a god or gods, especially belief in a personal God as creator and ruler of the world
-                    –cide ending originates from the Latin word caedere meaning to kill. 
-                    We want the world to know that we are comming for there gods and religion. We are comming for there ignorance they hold on so dearly to. It shall be eliminated. 
-                    Religion and the believe in god and or gods leads to a weakened critical thinking or allows one to accept things the way they are. 
-                    Conspiracy, and end of time predictions have allowed the churches to rule through fear, politics and crusades for far to long. Religion breeds racism, sexism, bigotry and hatred. This leaves no room for growth in ones self in this world. Religion and the beliefs in gods has allowed one and many more to not realize the abuse and manipulation they are experiencing and even more disgusting one may even accept that abuse and even welcome it. 
-                    This is why we must kill god and religion for the cancer that it is.
+
+                    To murder the Gods.
+
+                    Theocide is the systematic eradication of 
+                    a certain religion. 
+                    We want the Christian theist world to know that we are coming for their God and religion. We are coming for their ignorance they hold on so dearly to. 
+                    It shall be eliminated. Religion and the belief in God and or Gods leads to a weakened critical thinking or allows one to accept things the way they are. 
+                    When ultimately one will learn one must think for oneself! Conspiracy, end of time predictions have allowed the churches to rule through fear, politics and 
+                    crusades for far too long. Religion breeds racism, sexism, bigotry and hatred.
+                    During The Conquistadors' Crusades through the Americas, they committed theocide to well established civilizations such as the Aztecs and Native Americans and 
+                    murdered their gods. This is theocide for the worse due to a Christian theism. That is why our music's message has a strong anti-Christian/anti- organized religion 
+                    stance as Christian theology has repeatedly bred contempt and ignorance and has allowed cultures to wipe out other civilizations' cultures without any regard. 
+                    This is why we must kill the idea of singular "God" and religion for the cancer that it is.
                     <br/>
-                    Your Ignorance keeps you blind. Bring upon Theocide.
+                    Your ignorance keeps you blind. Bring upon Theocide.
+
 
                 </p>
 
